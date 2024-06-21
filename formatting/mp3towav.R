717552de8d820e3961ffeeb8bfad9bd22bcb03bf
kaiyaprovost <17089935+kaiyaprovost@users.noreply.github.com>
2024-06-21 16:02:31 -0400

    adding annotations

--- formatting/mp3towav.R
+++ formatting/mp3towav.R
@@ -2,62 +2,84 @@ library(tuneR)
 
 ## TODO: update so that formatted correctly
 
-#setwd("/users/PYS1065/kprovost/bioacoustics/Sounds_and_Annotations/Aves/")
-setwd("/Users/kprovost/Documents/Postdoc_Working/JY_Project/")
-mp3files = list.files(path=getwd(),
-                      pattern="\\.mp3$",full.names = T,recursive=T)
-mp3files=mp3files[!(grepl("WEIRD",mp3files))]
-mp3files=mp3files[!(grepl("weird",mp3files))]
-#mp3files=mp3files[!(grepl("Cardinalidae",mp3files))]
+## set our working directory that has the mp3 files in it
+setwd("/Users/kprovost/Documents/Chondestes_grammacus/")
 
-#x <- file.info(mp3files)
-#mp3files=mp3files[order(x$size)]
-for(mp3 in (mp3files)[length(mp3files):1]) {
-  if(!(file.exists(paste(mp3,".wav",sep="")))) {
+## get all the mp3 files in my folder
+## \\. means it's a real dot, not a wildcard
+## $ in the pattern means the end of the file
+mp3files = list.files(path=getwd(),
+                      pattern="\\.mp3$",full.names = TRUE,recursive=TRUE)
+
+## reverse the order of mp3files
+mp3files = mp3files[length(mp3files):1]
+
+## randomize the order of mp3files
+mp3files = sample(mp3files)
+
+## for loop to convert all the mp3s to wavs
+## if this fails, try scrambling the order
+## if that still fails, audacity
+for(mp3 in mp3files) {
+  wavfile = gsub("mp3","wav",mp3)
+  
+  ## ! turns TRUE into FALSE and FALSE into TRUE
+  ## if the wavfile does not exist already
+  if(!(file.exists(wavfile))) {
     print(mp3)
-    try({r <- readMP3(mp3)})  ## MP3 file in working directory
+    
+    ## try means try it out, and only do something if there is no error
+    
+    try({
+      r <- readMP3(mp3) ## read in the MP3 file in working directory
+      })  
+    
+    ## if the mp3 file is in stereo, convert it to mono
     if(r@stereo) {
-      try({r = mono(r, which = c("both"))}) ## this is making things bad
+      ## convert the mp3 to mono using an average of both left and right channels
+      try({r = mono(r, which = c("both"))}) 
     }
-    try({writeWave(r,paste(mp3,".wav",sep=""),extensible=FALSE)})
+    
+    ## output the wavfile to the path in "wavfile"
+    try({writeWave(r,wavfile,extensible=FALSE)})
   } else {print("skip")}
+  
+  ## try to compress the mp3 file to be smaller as a gzip
+  ## mp3.gz
   try({R.utils::gzip(mp3,overwrite=T)})
 }
 
-#setwd("/Users/kprovost/Documents/BLB_data/")
+## go get the list of the wav files
 wavfiles = list.files(path=getwd(),
-                      pattern="\\.wav$",full.names = T,recursive = T)
-wavfiles=wavfiles[!(grepl("copy",wavfiles))]
-wavfiles=wavfiles[!(grepl("BAD",wavfiles))]
-wavfiles=wavfiles[!(grepl("resample",wavfiles))]
-wavfiles=wavfiles[!(grepl("DONE",wavfiles))]
-wavfiles=wavfiles[!(grepl("tweetynet-ed",wavfiles))]
-#wavfiles=wavfiles[grepl("255595",wavfiles)]
+                      pattern="\\.wav$",full.names = TRUE,recursive = TRUE)
 
-#outfile="/Users/kprovost/Documents/BLB.sampling.rates.13may2022.txt"
 for(wav in (sort(wavfiles))) {
   print(wav)
   try({
+    ## try reading the file in 
     r <- tuneR::readWave(wav,from=0,units="seconds")
+    ## convert to mono if stereo
     if(r@stereo) {
-      try({r = mono(r, which = c("both"))}) ## this is making things bad?
+      try({r = mono(r, which = c("both"))}) 
     }
-    #if(r@samp.rate != 48000) {
+    ## 48000 is the most common (mode) sampling rate
+    ## convert the wav files into 48000 if needed
     if(sound::rate(wav) != 48000) {
-      
-      #r = sound::loadSample(wav)
-      #r <- tuneR::readWave(wav,from=0,units="seconds")
-      #print(r)
-      #r2=sound::setRate(r,48000) ## this causes too much distortion
-      #print(r2)
+      ## if it is not 48000, resample it to 48000
       r2=seewave::resamp(r,g=48000,output="Wave")
-      #print(wav)
-      #write(paste(wav,r@samp.rate,sep="\t"),outfile,append=T)
-      #sound::saveSample(r2,sub(".wav",".resample.48000.wav",wav),overwrite = T)
+      
+      ## write out the converted Wave, changing the name
       try({writeWave(r2,sub(".wav",".resample.48000.wav",wav),extensible=FALSE)})
+      
+      ## gzip the original wav file 
       try({R.utils::gzip(wav,overwrite=T)})
+      
     } else {
+      ## if it is ALREADY 48000
+      ## just going to output the file again, changing the name
       try({writeWave(r,sub(".wav",".resample.48000.wav",wav),extensible=FALSE)})
+      
+      ## gzip the original wav file
       try({R.utils::gzip(wav,overwrite=T)})
     }
   })
@@ -107,33 +129,5 @@ for(wavgz in rev(sort(wavgzfiles))) {
   }
 }
 
-
-
-# setwd("/Users/kprovost/Documents/XenoCanto/")
-# wavfiles = list.files(path="/Users/kprovost/Documents/XenoCanto/0_DONE/Trochilidae/Calypte/",
-#                       pattern="\\.wav$",full.names = T,recursive = T)
-# wavfiles=wavfiles[!(grepl("copy",wavfiles))]
-# wavfiles=wavfiles[!(grepl("BAD",wavfiles))]
-# 
-# outfile="sampling.rates.txt"
-# for(wav in rev(wavfiles)) {
-#   r <- readWave(wav,from=0,units="seconds")
-#   if(r@samp.rate != 48000) {
-#     print(wav)
-#     write(paste(wav,r@samp.rate,sep="\t"),outfile,append=T)
-#   }
-# }
-# 
-# for(wav in (wavfiles)) {
-#   
-#   try({r <- readWave(wav,from=0,units="seconds")  ## MP3 file in working directory
-#   })
-#   if(r@stereo) {
-#     print(wav)
-#     try({r = mono(r, which = c("both"))}) ## this is making things bad
-#     try({writeWave(r,paste(wav,".copy",sep=""),extensible=FALSE)})
-#   }
-#   #},error={print("oop"); next})
-# }
 
 
